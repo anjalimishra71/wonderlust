75aa7148cae1850dd002c20bfde554092a11647f
anjalimishra71 <anjalimishra310804@gmail.com>
2025-04-03 17:54:27 +0530

    add review section

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
+
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -31,12 +33,23 @@ app.use(express.static(path.join(__dirname, "/public")))
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
     if (error) {
-        let errMsg=error.details.map((el)=>el.message).join(",")
+        let errMsg = error.details.map((el) => el.message).join(",")
         throw new ExpressError(404, error);
     } else {
         next();
     }
 }
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(404, error);
+    } else {
+        next();
+    }
+}
+
 
 app.get("/", (req, res) => {
     res.send("working good...");
@@ -54,7 +67,7 @@ app.get("/listings/new", (req, res) => {
 
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     const { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     // console.log(listing);
     res.render("listings/show", { listing });
 }))
@@ -81,13 +94,43 @@ app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     res.redirect(`/listings/${id}`);
 }));
 
-
+//delete Route
 app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const deletedList = await Listing.findByIdAndDelete(id);
     console.log(deletedList);
     res.redirect("/listings");
 }))
+
+
+// post review(post route)
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+
+    console.log("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+})
+
+)
+
+//delete review route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async (req,res)=>{
+    let {id,reviewId}=req.params;
+
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+
+}))
+
+
+
 // app.get("/listTesting", async (req, res) => {
 //     let sampleListing = new Listing({
 //         title: "My new villa",
--- schema.js
+++ schema.js
@@ -1,4 +1,6 @@
-const Joi = require("joi");
+const Joi = require("joi"); //joi is use for sarver side validation
+const review = require("./models/review");
+
 
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
@@ -9,4 +11,11 @@ module.exports.listingSchema = Joi.object({
         price: Joi.number().required().min(0),
         Image: Joi.string().allow("", null)
     }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+    }).required(),
 });
